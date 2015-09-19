762fc4721623084b0c6737463cc49bdd63562ef8
Tim Wade <hello@timjwade.com>
2015-09-19 11:13:13 -0700

    Reverse the order of games
    For some reason PGN parses the file in reverse, so this restores the
    natural order of the games

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 Dir.glob(Rails.root.join("lib", "pgns", "*.pgn")).each do |pgn|
-  PGN.parse(File.read(pgn)).map do |game|
+  PGN.parse(File.read(pgn)).reverse_each do |game|
     Puzzle.create!(:fen => game.tags["FEN"], :answer => game.moves)
   end
 end
