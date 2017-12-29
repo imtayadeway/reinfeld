818be9eaa1d14f581cf774bf1a5a79fb31462b3f
Tim Wade <hello@timjwade.com>
2017-12-29 15:25:52 -0800

    Run app:update

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,7 @@
-ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
+
+if %w[s server c console].any? { |a| ARGV.include?(a) }
+  puts "=> Booting Rails"
+end
--- spec/factories.rb
+++ spec/factories.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
+  factory :book
+
+  factory :chapter do
+    book
+  end
+
   factory :puzzle do
+    book
+    chapter
     fen"3r2k1/1p5p/6p1/p2q1p2/P1Q5/1P5P/1P6/5RK1 w - - 0 1"
     solution %w(Rd1 Qxc4 Rxd8+ Kf7 bxc4)
   end
